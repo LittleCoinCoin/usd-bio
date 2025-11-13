57c4799ebb3d1cec01815b756e4b4feac6b42393
LittleCoinCoin <eliott.jacopin@laposte.net>
2025-11-13 23:15:52 +0900

    feat(core): add USD-Bio extension library with version API
    - Create src/include/usd_bio/extension.h with GetVersion() API
    - Implement GetVersion() in src/core/extension.cpp returning v0.1.0
    - Configure src/CMakeLists.txt for static library build with C++20
    - Link against OpenUSD via vcpkg pxr package

---
+++ src/include/usd_bio/extension.h
@@ -0,0 +1,19 @@
+#ifndef USD_BIO_EXTENSION_H
+#define USD_BIO_EXTENSION_H
+
+/*!
+@file extension.h
+@brief USD-Bio extension main entry point
+*/
+
+namespace usd_bio {
+    
+/*!
+@brief Get extension version string
+@return Version string in format "Major.Minor.Patch"
+*/
+const char* GetVersion();
+
+} // namespace usd_bio
+
+#endif // USD_BIO_EXTENSION_H
